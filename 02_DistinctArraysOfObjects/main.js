3b05c5632bcfe9c7427a9605b919d91a2e26bde3
Boris Brenner <63914278+nmboris@users.noreply.github.com>
2023-10-10 21:52:00 +0200

    add 02

---
+++ 02_DistinctArraysOfObjects/main.js
@@ -0,0 +1,54 @@
+/** @type {(Array<{host: string, path: string}>)} */
+const urls = [];
+
+// Generate a lot of random urls
+for (let i = 0; i < 10_000; i++) {
+	const random = "" + Math.floor(Math.random() * 1000) + 1;
+	urls.push({ host: random, path: "/" });
+}
+
+// Variant with for loop
+const variantA = () => {
+	/** @type {(Array<string>)} */
+	const distinctHostArray = [];
+
+	urls.forEach((host) => {
+		if (!distinctHostArray.includes(host.host)) {
+			distinctHostArray.push(host.host);
+		}
+	});
+
+	return distinctHostArray;
+};
+
+// Variant with map and filter
+const variantB = () => {
+	const distinctHostArray = urls
+		.map((host) => host.host)
+		.filter((value, index, self) => self.indexOf(value) === index);
+
+	return distinctHostArray;
+};
+
+// Variant with set
+const variantC = () => {
+	/** @type {(Set<string>)} */
+	const set = new Set();
+
+	urls.forEach((u) => set.add(u.host));
+
+	return [...set.keys()];
+};
+
+// Test
+console.time(":: variantA");
+variantA();
+console.timeEnd(":: variantA");
+
+console.time(":: variantB");
+variantB();
+console.timeEnd(":: variantB");
+
+console.time(":: variantC");
+variantC();
+console.timeLog(":: variantC");
